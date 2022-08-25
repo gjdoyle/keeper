77729c029ffde5fabde73723663b7fe66718afea
gjdoyle <gjdoyle@ksu.edu>
2022-08-25 10:13:12 -0500

    First

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <footer>
+        <p>GD Dev</p>
+    </footer>
+}
+
+export default Footer;
---
+++ src/note.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note (){
+    return <note>
+        <div className="note">
+            <h1>
+                Title
+            </h1>
+            <p> Content </p>
+        </div>
+    </note>
+}
+
+export default Note;
