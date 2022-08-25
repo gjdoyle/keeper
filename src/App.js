355b59da90b33db7eede7d7a2cda426a144a2be7
gjdoyle <gjdoyle@ksu.edu>
2022-08-25 11:21:46 -0500

    map, arrows, props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "./notes"
+
 
 function App(){
-  return <div>
+  return ( 
+  <div>
     <Header />
-    <Note />
+    {notes.map((noteItem) =>(
+    <Note 
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+    ))}
     <Footer />
   </div>
+  );
 }
 
 export default App;
